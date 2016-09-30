952b69b91b8c21f596fbaf05ce0c5e670c99c206
Jamie Haskell <jmehaskell@gmail.com>
2016-09-29 22:50:45 -0500

    Fixed repetitive task validation

--- js/jawasqwips.js
+++ js/jawasqwips.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
     // Initialize firebase
     var fireDB = firebase.database();
     var new_task_id = '';
-    var current_tasks = [$('#defaultTask').find('.task-text').text()];
+    var current_tasks = [$('#defaultTask').find('.task-text').text().toLowerCase()];
 
     function addTask(newTask)
     {
@@ -58,6 +58,7 @@ $(document).ready(function(){
 
         promise.then(function(value){
             //promise was fullfilled
+            current_tasks.push(data.description.toLowerCase());
             $('input#newTask').val('');
             $('.task-added.alert-success').fadeIn();
             $('ul').append('<li data-task-id=' + new_task_id + '><div class="task-display"><span class="input-group-addon"><input type="checkbox" aria-label="task-complete"></span><span class="task-text">' + data.description + ' </span><i class="fa fa-pencil" data-toggle="tooltip" data-placement="top" title="Edit task"></i><i class="fa fa-remove" data-toggle="tooltip" data-placement="right" title="Remove task"></i></div><div class="task-edit" style="display:none;"><input type="text" name="edit-' + new_task_id + '" value="' + data.description + '" class="form-control"><span class="edit-icons"><i class="fa fa-undo" data-toggle="tooltip" data-placement="top" title="Undo"></i><i class="fa fa-save" data-toggle="tooltip" data-placement="right" title="Save task"></i></span></div></li>')
@@ -72,23 +73,42 @@ $(document).ready(function(){
     function editTask(list_item)
     {
         var task_id = list_item.attr('data-task-id');
-        var new_description = list_item.find('input[name="edit-' + task_id + '"]').val();
-        var data = {description: new_description};
-        var promise = fireDB.ref('tasks/' + task_id).update(data);
+        var current_description = list_item.find('.task-text').text().toLowerCase().trim();
+        var new_description = list_item.find('input[name="edit-' + task_id + '"]').val().replace(/<(?:.|\n)*?>/gm, '').trim();
 
-        promise.then(function(value){
-            //promise was fullfilled
+        if(new_description != '') {
+            var data = {description: new_description};
+            var promise = fireDB.ref('tasks/' + task_id).update(data);
+            var index = $.inArray(current_description, current_tasks);
+            var exists = $.inArray(new_description, current_tasks);
+            
+            if(exists >= 0 && index != exists){
+                //task already exists in current to-do list
+                $('.task-update.alert-danger').html("<strong>Woops</strong>! Task already exists.").fadeIn();
+                setTimeout(function(){ $('.alert').fadeOut('slow'); }, 2000);
+            }
+            else {
+                promise.then(function(value){
+                    //promise was fullfilled
+                    current_tasks[index] = new_description.toLowerCase();
+                    list_item.find('.task-edit').fadeOut(400, function(){
+                        $(this).siblings('.task-display').find('.task-text').text(new_description);
+                        $(this).siblings('.task-display').fadeIn();
+                    });
+                    $('.task-update.alert-success').html("<strong>Woot!</strong> Task edited successfully!").fadeIn();
+                }, function(error){
+                    //promise was rejected
+                    $('.task-update.alert-danger').html("<strong>Woops</strong>! Unable to update task. Please try again.").fadeIn();
+                });
+            }
+            setTimeout(function(){ $('.alert').fadeOut('slow'); }, 2000);
+        }
+        else {
             list_item.find('.task-edit').fadeOut(400, function(){
-                $(this).siblings('.task-display').find('.task-text').text(new_description);
                 $(this).siblings('.task-display').fadeIn();
+                $(this).find('input').val(current_description);
             });
-            $('.task-update.alert-success').html("<strong>Woot!</strong> Task edited successfully!").fadeIn();
-        }, function(error){
-            //promise was rejected
-            $('.task-update.alert-danger').html("<strong>Woops</strong>! Unable to update task. Please try again.").fadeIn();
-        });
-
-        setTimeout(function(){ $('.alert').fadeOut('slow'); }, 2000);
+        }
     }
 
     // Utilizing soft-deletion
@@ -145,7 +165,7 @@ $(document).ready(function(){
         //loop through all tasks and append those that are not deleted
         $.each(snapshot.val(), function(task_id, task_obj){
             if(!task_obj.deleted) {
-                current_tasks.push(task_obj.description);
+                current_tasks.push(task_obj.description.toLowerCase());
                 $('ul').append('<li data-task-id=' + task_id + '><div class="task-display"><span class="input-group-addon"><input type="checkbox" aria-label="task-complete"></span><span class="task-text">' + task_obj.description + ' </span><i class="fa fa-pencil" data-toggle="tooltip" data-placement="top" title="Edit task"></i><i class="fa fa-remove" data-toggle="tooltip" data-placement="right" title="Remove task"></i></div><div class="task-edit" style="display:none;"><input type="text" name="edit-' + task_id + '" value="' + task_obj.description + '" class="form-control"><span class="edit-icons"><i class="fa fa-undo" data-toggle="tooltip" data-placement="top" title="Undo"></i><i class="fa fa-save" data-toggle="tooltip" data-placement="right" title="Save task"></i></span></div></li>')
                 if(task_obj.complete){
                     $('li[data-task-id="' + task_id + '"]').addClass('complete').find('input').attr('checked', 'checked');
@@ -162,10 +182,10 @@ $(document).ready(function(){
 
         var newTask = $('input#newTask').val();
         // Strip tags
-        newTask = newTask.replace(/<(?:.|\n)*?>/gm, '');
+        newTask = newTask.replace(/<(?:.|\n)*?>/gm, '').trim();
 
         if(newTask != '') {
-            if($.inArray(newTask, current_tasks)){
+            if($.inArray(newTask.toLowerCase(), current_tasks) >= 0){
                 //task already exists in current to-do list
                 $('.task-update.alert-danger').html("<strong>Woops</strong>! Task already exists.").fadeIn();
                 setTimeout(function(){ $('.alert').fadeOut('slow'); }, 2000);
